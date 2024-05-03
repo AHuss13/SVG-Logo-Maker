c1c4cdefa0cde750c71eb4b798a510bea1bed8af
Adam Huss <adamhuss13@gmail.com>
2024-05-02 20:56:34 -0500

    outlined index and shapes.js

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+// {
+//   type: 'input',
+//   name: 'text',
+//   message: 'Enter text (up to three characters):',
+// },
+// {
+//   type: 'input',
+//   name: 'textColor',
+//   message: 'Enter text color (keyword or hexadecimal):',
+// },
+// {
+//   type: 'list',
+//   name: 'shape',
+//   message: 'Choose a shape:',
+//   choices: ['Circle', 'Triangle', 'Square'],
+// },
+// {
+//   type: 'input',
+//   name: 'shapeColor',
+//   message: 'Enter shape color (keyword or hexadecimal):',
+// }
